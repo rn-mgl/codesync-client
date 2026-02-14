cf9568c0075f0cd72e99e36a9ccc0f77acdbdfa5
rn-mgl <reinb2ra@gmail.com>
2026-02-14 14:03:26 +0800

    remove log

--- app/auth/reset/page.tsx
+++ app/auth/reset/page.tsx
@@ -39,8 +39,6 @@ const Reset = () => {
     });
   };
 
-  console.log(showPassword);
-
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center bg-primary p-4 t:p-8">
       <div className="w-full max-w-(--breakpoint-l-s) bg-secondary h-full rounded-2xl grid grid-cols-1 l-s:grid-cols-2 p-2 gap-10">
