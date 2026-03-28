8e71aa0b6a7c9535f4d921585b7ac9f267f75db0
rn-mgl <reinb2ra@gmail.com>
2026-03-28 18:10:36 +0800

    comment

--- src/utils/problem.util.ts
+++ src/utils/problem.util.ts
@@ -1,9 +1,9 @@
 import { InputFormat } from "../interfaces/problem.interface";
 
 export const generateBoilerPlate = (inputFormat: InputFormat) => {
-  const style = inputFormat.style;
-  const name = inputFormat.name;
-  const parameters = inputFormat.params.map((param) => param.name).join(", ");
+  const style = inputFormat.style; // function or class
+  const name = inputFormat.name; // function or class name
+  const parameters = inputFormat.params.map((param) => param.name).join(", "); // parameters
 
   const boilerPlate = `${style} ${name} (${parameters}) {\n\t\n}`;
 
