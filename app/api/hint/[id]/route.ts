36c686e708a23bbed20bdadd37dc1c26689e0fda
rn-mgl <reinb2ra@gmail.com>
2026-05-19 20:29:18 +0800

    template for single crud

---
+++ app/api/hint/[id]/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+  } catch (error) {}
+}
+
+export async function PATCH(req: NextRequest) {
+  try {
+  } catch (error) {}
+}
+
+export async function DELETE(req: NextRequest) {
+  try {
+  } catch (error) {}
+}
