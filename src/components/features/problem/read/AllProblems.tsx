7b863054ea6f70d655433c6986bd2e36ddef4fbd
rn-mgl <reinb2ra@gmail.com>
2026-07-10 19:15:45 +0800

    color indicators

--- src/components/features/problem/read/AllProblems.tsx
+++ src/components/features/problem/read/AllProblems.tsx
@@ -14,23 +14,55 @@ const AllProblems = () => {
 
   useSession({ required: true });
 
+  const DIFFICULTY_COLOR = {
+    easy: "var(--color-green-600)",
+    medium: "var(--color-amber-600)",
+    hard: "var(--color-red-600)",
+  };
+
+  const ACCEPTANCE_COLOR = {
+    low: "var(--color-red-600)",
+    average: "var(--color-amber-600)",
+    high: "var(--color-green-600)",
+  };
+
   const mappedProblems = problems.map((problem) => {
+    const rate =
+      problem.acceptance_rate < 50
+        ? "low"
+        : problem.acceptance_rate < 75
+          ? "average"
+          : "high";
+
     return (
       <div
         key={problem.id}
         className="w-full not-last:border-b-2 border-neutral-400 transition-all
                   hover:bg-neutral-200 first:rounded-t-md last:rounded-b-md"
       >
-        <div className="grid grid-cols-4 w-full p-4 gap-4 text-sm">
+        <div className="grid grid-cols-4 w-full p-4 gap-4 text-sm items-center">
           <p>{problem.id}</p>
           <Link
             href={`/codesync/problems/${problem.slug}`}
-            className="hover:underline underline-offset-2"
+            className="hover:underline underline-offset-2 truncate"
           >
             {problem.title}
           </Link>
-          <p>{problem.difficulty}</p>
-          <p>{problem.acceptance_rate}</p>
+          <p
+            style={{ background: DIFFICULTY_COLOR[problem.difficulty] }}
+            className="w-fit rounded-full px-2 py-0.5 text-secondary text-xs capitalize"
+          >
+            {problem.difficulty}
+          </p>
+          <div className="p-1 h-fit rounded-full w-full relative bg-neutral-300 flex flex-col items-start justify-start">
+            <div
+              style={{
+                width: `${problem.acceptance_rate}%`,
+                background: ACCEPTANCE_COLOR[rate],
+              }}
+              className=" h-full absolute rounded-full top-0 left-0"
+            ></div>
+          </div>
         </div>
       </div>
     );
