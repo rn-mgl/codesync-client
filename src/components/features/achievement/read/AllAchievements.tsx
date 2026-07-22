cb55984259c6880b3d187032faa5af9a3c16ff9a
rn-mgl <reinb2ra@gmail.com>
2026-07-22 19:00:47 +0800

    fix

--- src/components/features/achievement/read/AllAchievements.tsx
+++ src/components/features/achievement/read/AllAchievements.tsx
@@ -50,7 +50,7 @@ const AllAchievements = () => {
           <div className="w-full flex items-center justify-center h-full">
             <div
               dangerouslySetInnerHTML={{ __html: achievement.icon }}
-              className="w-full h-full flex items-center justify-center"
+              className="w-full h-full flex items-center justify-center relative"
             />
           </div>
         </div>
