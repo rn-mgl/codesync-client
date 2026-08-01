9acb71d7ba465564cc757e98a89466c3636def41
rn-mgl <reinb2ra@gmail.com>
2026-08-01 14:28:30 +0800

    fix state

--- src/hooks/usePaginate.tsx
+++ src/hooks/usePaginate.tsx
@@ -1,17 +1,41 @@
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 export default function usePaginate(initialLimit?: number) {
-  const [page, setPage] = React.useState(0);
-  const [limit, setLimit] = React.useState(initialLimit ?? 25);
   const [pages, setPages] = React.useState(0);
   const [canSelectLimit, setCanSelectLimit] = React.useState(false);
 
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const urlPage = searchParams?.get("page");
+  const urlLimit = searchParams?.get("limit");
+
+  const page = urlPage ? Number(urlPage) : 0;
+
+  const limit = urlLimit ? Number(urlLimit) : (initialLimit ?? 25);
+
+  const syncPaginateChange = React.useCallback(
+    (page: number, limit: number) => {
+      const params = new URLSearchParams(searchParams?.toString());
+
+      params.set("page", String(page));
+      params.set("limit", String(limit));
+
+      router.replace(`${pathname}?${params.toString()}`, {
+        scroll: false,
+      });
+    },
+    [pathname, router, searchParams],
+  );
+
   const handleLimit = (limit: number) => {
-    setLimit(limit);
+    syncPaginateChange(page, limit);
   };
 
   const handlePage = (page: number) => {
-    setPage(page);
+    syncPaginateChange(page, limit);
   };
 
   const handlePages = React.useCallback((pages: number) => {
